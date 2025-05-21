940fa27186c9785964b1d2dfe5b4597c9d8700da
HoaiAn2807 <hoaian280705@gmail.com>
2025-05-22 02:50:50 +0700

    Add files via upload

---
+++ bandienthoai/bandienthoai/assets/js/product.js
@@ -0,0 +1,53 @@
+
+const products = {
+    iphone: [
+        { name: "iPhone 12", price: "15.000.000₫", image: "./assets/img/sanphamip/ip12.png" },
+        { name: "iPhone 13", price: "17.000.000₫", image: "./assets/img/sanphamip/iphone-13-pro-max.png" },
+        { name: "iPhone 14", price: "19.000.000₫", image: "./assets/img/sanphamip/iphone-14_1.png" },
+        { name: "iPhone 15", price: "21.000.000₫", image: "./assets/img/sanphamip/iphone-15-plus_1__1.png" },
+        { name: "iPhone 16", price: "25.000.000₫", image: "./assets/img/sanphamip/iphone-16-pro-max.png" },
+    ],
+    oppo: [
+        { name: "OPPO FIND N5", price: "3.490.000₫", image: "./assets/img/sanphamop/dien-thoai-oppo-find-n5_h_nh_2.png" },
+        { name: "OPPO RENO 13F", price: "4.990.000₫", image: "./assets/img/sanphamop/dien-thoai-oppo-reno13f-4g_2_.png" },
+        { name: "OPPO A55", price: "8.990.000₫", image: "./assets/img/sanphamop/oppo-a55_1_.png" },
+        { name: "OPPO A78", price: "18.990.000₫", image: "./assets/img/sanphamop/oppo-a78.png" },
+        { name: "OPPO A3", price: "5.490.000₫", image: "./assets/img/sanphamop/text_ng_n_7__2_102.png" },
+    ],
+    samsung: [
+        { name: "Samsung Galaxy A06", price: "21.990.000₫", image: "./assets/img/sanphamss/dien-thoai-samsung-galaxy-a06-5g_1__1.png" },
+        { name: "Samsung Galaxy A16", price: "25.990.000₫", image: "./assets/img/sanphamss/dien-thoai-samsung-galaxy-a16_1__3.png" },
+        { name: "Samsung Galaxy A36", price: "11.490.000₫", image: "./assets/img/sanphamss/dien-thoai-samsung-galaxy-a36.2.png" },
+        { name: "Samsung Galaxy A56", price: "27.990.000₫", image: "./assets/img/sanphamss/dien-thoai-samsung-galaxy-a56.1_1.png" },
+        { name: "Samsung Galaxy Z FOLD", price: "7.990.000₫", image: "./assets/img/sanphamss/samsung-galaxy-z-fold-6-xanh_5_.png" },
+    ],
+    realme: [
+        { name: "Realme C61", price: "12.990.000₫", image: "./assets/img/sanphamrm/dien-thoai-realme-c61_2__2.png" },
+        { name: "Realme C55", price: "4.990.000₫", image: "./assets/img/sanphamrm/dien-thoai-realme-c65s_3_.png" },
+        { name: "Realme NOTE 60", price: "8.490.000₫", image: "./assets/img/sanphamrm/dien-thoai-realme-note-60-4gb-64gb_1.png" },
+        { name: "Realme NOTE 60X", price: "5.990.000₫", image: "./assets/img/sanphamrm/dien-thoai-realme-note-60x.png" },
+        { name: "Realme 13 PLUS", price: "6.490.000₫", image: "./assets/img/sanphamrm/realme-13-plus-5g_6_.png" },
+    ],
+    xiaomi: [
+        { name: "Xiaomi POCO", price: "18.990.000₫", image: "./assets/img/sanphamxm/dien-thoai-poco-x7-pro-5g_1_.png" },
+        { name: "Xiaomi 15", price: "6.490.000₫", image: "./assets/img/sanphamxm/dien-thoai-xiaomi-15_11_.png" },
+        { name: "Xiaomi REDMI NOTE 14", price: "9.490.000₫", image: "./assets/img/sanphamxm/dien-thoai-xiaomi-redmi-note-14_2__2.png" },
+        { name: "Xiaomi 15 ULTRA", price: "12.990.000₫", image: "./assets/img/sanphamxm/photo_2025-04-16_11-45-37.png" },
+        { name: "Xiaomi 14T", price: "28.990.000₫", image: "./assets/img/sanphamxm/xiaomi_14t_2_.png" },
+    ],
+    vivo: [
+        { name: "Vivo X80 Pro", price: "16.990.000₫", image: "./assets/img/sanphamvivo/x80pro.png" },
+        { name: "Vivo V27", price: "9.490.000₫", image: "./assets/img/sanphamvivo/v27.png" },
+        { name: "Vivo Y100", price: "5.990.000₫", image: "./assets/img/sanphamvivo/y100.png" },
+        { name: "Vivo T1 Pro", price: "8.990.000₫", image: "./assets/img/sanphamvivo/t1pro.png" },
+        { name: "Vivo X Fold", price: "30.990.000₫", image: "./assets/img/sanphamvivo/xfold.png" },
+    ],
+
+    noibat: [
+        { name: "Realme NOTE 60", price: "8.490.000₫", image: "./assets/img/sanphamrm/dien-thoai-realme-note-60-4gb-64gb_1.png" },
+        { name: "Samsung Galaxy A06", price: "21.990.000₫", image: "./assets/img/sanphamss/dien-thoai-samsung-galaxy-a06-5g_1__1.png" },
+        { name: "Realme C61", price: "12.990.000₫", image: "./assets/img/sanphamrm/dien-thoai-realme-c61_2__2.png" },
+        { name: "OPPO FIND N5", price: "3.490.000₫", image: "./assets/img/sanphamop/dien-thoai-oppo-find-n5_h_nh_2.png" },
+        { name: "Xiaomi REDMI NOTE 14", price: "9.490.000₫", image: "./assets/img/sanphamxm/dien-thoai-xiaomi-redmi-note-14_2__2.png" },
+    ]
+};
